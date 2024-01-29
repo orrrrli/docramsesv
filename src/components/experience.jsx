0e07feda51da79b8d3e7c4df175269644d6c4596
orrrrli <castaneda.kevin@uabc.edu.mx>
2024-01-29 13:41:16 -0800

    info update in the experience section

--- src/components/experience.jsx
+++ src/components/experience.jsx
@@ -13,10 +13,12 @@ function Experience () {
                     </div>
                     <div className="block p-6 rounded-lg shadow-lg bg-white-50 max-w-md ml-6 mb-10">
                         <div className="flex justify-between mb-4">
-                        <a href="#!" className="font-medium text-sky-600">New Web Design</a>
-                        <a href="#!" className="font-medium text-sky-600">04 / 02 / 2022</a>
+                        <a className="font-medium text-sky-600 mr-6">Titulación. Médico Cirujano y Partero</a>
+                        <a className="font-medium text-sky-600">09/09/2010</a>
                         </div>
-                        <p className="text-gray-700 mb-6">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque scelerisque diam non nisi semper, et elementum lorem ornare. Maecenas placerat facilisis mollis. Duis sagittis ligula in sodales vehicula.</p>
+                        <p className="text-gray-700 mb-6">Vía EGEL/CENEVAL (28/05/2010)<br></br>
+                        Tepic, Nayarit
+                        </p>
                     </div>
                 </div>
                 <div className="md:flex justify-center">
@@ -28,10 +30,12 @@ function Experience () {
                     </div>
                     <div className="block p-6 rounded-lg shadow-lg bg-white-50 max-w-md ml-6 mb-10">
                         <div className="flex justify-between mb-4">
-                        <a href="#!" className="font-medium text-sky-600">New Web Design</a>
-                        <a href="#!" className="font-medium text-sky-600">04 / 02 / 2022</a>
+                        <a className="font-medium text-sky-600">International Diabetes Center / ReMeDi (Resultados Médicos, Educación e Investigación en Salud, A. C.)</a>
+                        <a className="font-medium text-sky-600">Noviembre 2012</a>
                         </div>
-                        <p className="text-gray-700 mb-6">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque scelerisque diam non nisi semper, et elementum lorem ornare. Maecenas placerat facilisis mollis. Duis sagittis ligula in sodales vehicula.</p>
+                        <p className="text-gray-700 mb-6">Curso de BASIC, Manejo de Diabetes por Etapas 2012
+                        Cd. de México D.F.
+                        </p>
                     </div>
                 </div>
                 <div className="md:flex justify-center">
@@ -43,10 +47,50 @@ function Experience () {
                     </div>
                     <div className="block p-6 rounded-lg shadow-lg bg-white-50 max-w-md ml-6 mb-10">
                         <div className="flex justify-between mb-4">
-                        <a href="#!" className="font-medium text-sky-600">New Web Design</a>
-                        <a href="#!" className="font-medium text-sky-600">04 / 02 / 2022</a>
+                        <a className="font-medium text-sky-600">e-REMEDI (Electronic - Resultados Médicos, Educación e Investigación en Salud, A. C.)</a>
+                        <a className="font-medium text-sky-600">14/12/2012</a>
                         </div>
-                        <p className="text-gray-700 mb-6">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque scelerisque diam non nisi semper, et elementum lorem ornare. Maecenas placerat facilisis mollis. Duis sagittis ligula in sodales vehicula.</p>
+                        <p className="text-gray-700 mb-6">Diplomado: Organización, Desarrollo y Acción Grupal en Diabetes Mellitus tipo 2
+                        <br></br>A distancia
+                        <br></br>01/11/2012 – 14/12/2012 (Calif: 10) 
+                        .</p>
+                    </div>
+                </div>
+                <div className="md:flex justify-center">
+                    <li className="border-l-2 border-sky-700"></li>
+                    <div className="bg-sky-800 w-6 h-6 flex items-center justify-center rounded-full -ml-3.5">
+                        <svg aria-hidden="true" focusable="false" data-prefix="fas" className="text-white w-3 h-3" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+                        <path fill="currentColor" d="M0 464c0 26.5 21.5 48 48 48h352c26.5 0 48-21.5 48-48V192H0v272zm64-192c0-8.8 7.2-16 16-16h288c8.8 0 16 7.2 16 16v64c0 8.8-7.2 16-16 16H80c-8.8 0-16-7.2-16-16v-64zM400 64h-48V16c0-8.8-7.2-16-16-16h-32c-8.8 0-16 7.2-16 16v48H160V16c0-8.8-7.2-16-16-16h-32c-8.8 0-16 7.2-16 16v48H48C21.5 64 0 85.5 0 112v48h448v-48c0-26.5-21.5-48-48-48z"></path>
+                        </svg>
+                    </div>
+                    <div className="block p-6 rounded-lg shadow-lg bg-white-50 max-w-md ml-6 mb-10">
+                        <div className="flex justify-between mb-4">
+                        <a className="font-medium text-sky-600 mr-2">ISSSTE (Instituto de Seguridad y Servicios Sociales de los Trabajadores del Estado)</a>
+                        <a className="font-medium text-sky-600">29/11/2012</a>
+                        </div>
+                        <p className="text-gray-700 mb-6">Curso-Taller: Síndrome Cardiometabólico
+                        <br></br>Ensenada, Baja California
+                        <br></br>28/11/2012 - 29/11/2012 (16 horas)
+                        .</p>
+                    </div>
+                </div>
+                <div className="md:flex justify-center">
+                    <li className="border-l-2 border-sky-700"></li>
+                    <div className="bg-sky-800 w-6 h-6 flex items-center justify-center rounded-full -ml-3.5">
+                        <svg aria-hidden="true" focusable="false" data-prefix="fas" className="text-white w-3 h-3" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+                        <path fill="currentColor" d="M0 464c0 26.5 21.5 48 48 48h352c26.5 0 48-21.5 48-48V192H0v272zm64-192c0-8.8 7.2-16 16-16h288c8.8 0 16 7.2 16 16v64c0 8.8-7.2 16-16 16H80c-8.8 0-16-7.2-16-16v-64zM400 64h-48V16c0-8.8-7.2-16-16-16h-32c-8.8 0-16 7.2-16 16v48H160V16c0-8.8-7.2-16-16-16h-32c-8.8 0-16 7.2-16 16v48H48C21.5 64 0 85.5 0 112v48h448v-48c0-26.5-21.5-48-48-48z"></path>
+                        </svg>
+                    </div>
+                    <div className="block p-6 rounded-lg shadow-lg bg-white-50 max-w-md ml-6 mb-10">
+                        <div className="flex justify-between mb-4">
+                        <a className="font-medium text-sky-600 mr-2">Asociación Mazatleca para la Diabetes A.C. / UAS (Universidad Autónoma de Sinaloa)</a>
+                        <a className="font-medium text-sky-600">03/08/2013</a>
+                        </div>
+                        <p className="text-gray-700 mb-6">Asistente: IX Megasimposium de Diabetes Mellitus
+                        <br></br>Mazatlán, Sinaloa
+                        <br></br>02/08/2013 - 03/08/2013 (25 horas)
+
+                        .</p>
                     </div>
                 </div>
             </ol>
