15b2c99843a06dcab72fc0f8b81c8fdcaac6938a
orrrrli <castaneda.kevin@uabc.edu.mx>
2024-01-28 17:01:44 -0800

    slider implemented

--- src/components/images.jsx
+++ src/components/images.jsx
@@ -1,65 +1,78 @@
-function Images (){
+import { useState } from 'react';
+import { BsChevronCompactLeft, BsChevronCompactRight } from 'react-icons/bs';
+import { RxDotFilled } from 'react-icons/rx';
+
+function Images() {
+  const slides = [
+    {
+      url: 'third.jpg',
+    },
+    {
+      url: 'fiveone.jpg',
+    },
+    {
+      url: 'fourth.jpg',
+    },
+
+    {
+      url: 'first.jpg',
+    },
+    {
+      url: 'second.jpg',
+    },
+    {
+      url: 'sixth.jpg',
+    },
+    {
+      url: 'sevenone.jpg',
+    }
+  ];
+
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const prevSlide = () => {
+    const isFirstSlide = currentIndex === 0;
+    const newIndex = isFirstSlide ? slides.length - 1 : currentIndex - 1;
+    setCurrentIndex(newIndex);
+  };
+
+  const nextSlide = () => {
+    const isLastSlide = currentIndex === slides.length - 1;
+    const newIndex = isLastSlide ? 0 : currentIndex + 1;
+    setCurrentIndex(newIndex);
+  };
+
+  const goToSlide = (slideIndex) => {
+    setCurrentIndex(slideIndex);
+  };
+
   return (
-    <div className="container mx-auto p-4">
-      <div>
-            <img
-              className="h-auto max-w-full rounded-lg"
-              src="third.jpg"
-              alt=""
-            />
+    <div className='max-w-96 h-96 md:max-w-[1000px] md:h-[780px] w-full m-auto py-16 px-4 group'>
+      <div
+        style={{ backgroundImage: `url(${slides[currentIndex].url})` }}
+        className='w-full h-full rounded-2xl bg-center bg-cover duration-500'
+      ></div>
+      {/* Left Arrow */}
+      <div className='hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] left-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer'>
+        <BsChevronCompactLeft onClick={prevSlide} size={30} />
+      </div>
+      {/* Right Arrow */}
+      <div className='hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] right-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer'>
+        <BsChevronCompactRight onClick={nextSlide} size={30} />
+      </div>
+      <div className='flex top-4 justify-center py-2'>
+        {slides.map((slide, slideIndex) => (
+          <div
+            key={slideIndex}
+            onClick={() => goToSlide(slideIndex)}
+            className='text-2xl cursor-pointer'
+          >
+            <RxDotFilled />
           </div>
-      <div className="grid grid-cols-1 md:grid-cols-1 gap-2">
-        <div className="grid gap-4">
-          <div>
-            <img
-              className="h-auto max-w-full rounded-lg"
-              src="first.jpg"
-              alt=""
-            />
-          </div>
-          <div>
-            <img
-              className="h-auto max-w-full rounded-lg"
-              src="second.jpg"
-              alt=""
-            />
-          </div>
-        </div>
-        <div className="grid gap-4">
-          <div>
-            <img
-              className="h-auto max-w-full rounded-lg"
-              src="fourth.jpg"
-              alt=""
-            />
-          </div>
-          <div>
-            <img
-              className="h-auto max-w-full rounded-lg"
-              src="fiveone.jpg"
-              alt=""
-            />
-          </div>
-          <div>
-            <img
-              className="h-auto max-w-full rounded-lg"
-              src="sixth.jpg"
-              alt=""
-            />
-          </div>
-        </div>
-        <div className="grid gap-4">
-          <div>
-            <img
-              className="h-auto max-w-full rounded-lg"
-              src="sevenone.jpg"
-              alt=""
-            />
-          </div>
-        </div>
+        ))}
       </div>
     </div>
-  )
+  );
 }
 
-export default Images
+export default Images;
